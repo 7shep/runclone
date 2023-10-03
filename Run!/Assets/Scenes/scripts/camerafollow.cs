98362560d2f91f5dcd2ae89a45bcda45ca5ff91c
NickAlies <146098566+NickAlies@users.noreply.github.com>
2023-10-03 10:09:53 -0400

    s

---
+++ Run!/Assets/Scenes/scripts/camerafollow.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject player;
+    public float distanceFromPlayer = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // forces the camera behind the player at all times
+        transform.position = player.transform.position - player.transform.forward *distanceFromPlayer;
+        transform.LookAt(player.transform.position);
+    }
+}
---
+++ Run!/Assets/Scenes/scripts/playercontroller.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController   : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 3;
+    public float roationspeed = 90;
+    public float gravity = -20;
+    public float jumpspeed = 15;
+
+    CharacterController characterController;
+    Vector3 moveVelocity;
+    Vector3 turnVelocity;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var hInput = Input.GetAxis("Horizontal");
+        var vinput = Input.GetAxis("Vertical");
+
+        if(characterController.isGrounded)
+        { //this is the script allowing the character to group when on ground
+            moveVelocity = transform.forward * speed * vinput;
+            turnVelocity = transform.up * roationspeed * hInput;
+                if(Input.GetButtonDown("Jump"))
+                {
+                moveVelocity.y = jumpspeed;
+            }
+        }
+        //Adding gravity
+        moveVelocity.y += gravity * Time.deltaTime;
+        characterController.Move(moveVelocity * Time.deltaTime);
+        transform.Rotate(turnVelocity * Time.deltaTime);
+    }
+}
