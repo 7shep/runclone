13923c322a3dc137ad3ee85d3ab75d0fe723853c
Alex <68706402+7shep@users.noreply.github.com>
2023-10-06 10:17:23 -0400

    te

--- Run!/Assets/Scripts/groundspawner.cs
+++ Run!/Assets/Scripts/groundspawner.cs
@@ -1,35 +1,19 @@
-using UnityEngine;
+﻿using UnityEngine;
 
-// This is the script for spawning the tiles, the only thing that is used is the spawnTile function. This will be used in other scripts (GroundTiletile.cs)
-
-public class groundspawner : MonoBehaviour
-{
+public class GroundSpawner : MonoBehaviour {
 
     public GameObject groundTile;
     Vector3 nextSpawnPoint;
 
-
-    public void spawnTile()
+    public void SpawnTile ()
     {
-        //Function is "temp" allowing me to manipulate and reference it.
         GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
 
-
-        //Updates the position for the next ground tile to be spawned.
-        nextSpawnPoint = temp.transform.GetChild(1).transform.position;   
-
+        nextSpawnPoint = temp.transform.GetChild(1).transform.position;
     }
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        //Constantly spawns tiles.
-        for (int i = 0; i < 15; i++)
-        {
-
-            spawnTile();
-
+    private void Start () {
+        for (int i = 0; i < 15; i++) {
+            SpawnTile();
         }
     }
-
-    }
+}
